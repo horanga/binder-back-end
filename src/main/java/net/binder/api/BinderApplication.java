f4c2240fa90a744b9e84c41bc621da2cee086390
sangchu <didi1484@gmail.com>
2024-08-29 22:38:17 +0900

    init: 프로젝트 생성 및 초기 설정

---
+++ src/main/java/net/binder/api/BinderApplication.java
@@ -0,0 +1,13 @@
+package net.binder.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/binder/api/BinderApplicationTests.java
@@ -0,0 +1,13 @@
+package net.binder.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
