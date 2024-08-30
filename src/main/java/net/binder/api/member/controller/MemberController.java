2c376aa5d23c00f7ea25b78fe701e9a48bea40cf
sangchu <didi1484@gmail.com>
2024-08-30 20:07:34 +0900

    feat: 로그인된 사용자 프로필 조회 구현

---
+++ src/main/java/net/binder/api/member/controller/MemberController.java
@@ -0,0 +1,32 @@
+package net.binder.api.member.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import net.binder.api.auth.dto.CustomOAuth2User;
+import net.binder.api.member.dto.MemberDetailResponse;
+import net.binder.api.member.entity.Member;
+import net.binder.api.member.service.MemberService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/members")
+@Tag(name = "회원 관리")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Operation(summary = "본인 정보 조회")
+    @GetMapping("/me")
+    public MemberDetailResponse profile(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        String email = customOAuth2User.getName();
+
+        Member member = memberService.findByEmail(email);
+
+        return MemberDetailResponse.from(member);
+    }
+}
