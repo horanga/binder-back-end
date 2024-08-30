1b57a5adc25d726fe2c65514785a51b808623360
sangchu <didi1484@gmail.com>
2024-08-30 20:08:07 +0900

    fix: HomeController 스웨거에 노출 제외

--- src/main/java/net/binder/api/auth/controller/HomeController.java
+++ src/main/java/net/binder/api/auth/controller/HomeController.java
@@ -1,11 +1,14 @@
 package net.binder.api.auth.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Hidden
 public class HomeController {
 
+    // AWS ELB health 응답용 API
     @GetMapping("/")
     public void home() {
     }
