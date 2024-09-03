0d7ea196a18da1d234a16d4197c142b239397f63
sangchu <didi1484@gmail.com>
2024-09-03 14:31:15 +0900

    feat: 로그아웃 기능 구현

---
+++ src/main/java/net/binder/api/auth/controller/AuthController.java
@@ -0,0 +1,27 @@
+package net.binder.api.auth.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Operation(summary = "로그아웃")
+    @PostMapping("/logout")
+    public void logout(HttpServletResponse response) {
+
+        Cookie cookie = new Cookie(HttpHeaders.AUTHORIZATION, null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+
+        response.addCookie(cookie);
+    }
+}
