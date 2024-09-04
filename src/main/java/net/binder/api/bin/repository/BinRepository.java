74605e0b5d71d6a1533fee49c95a532fccf1b1af
sangchu <didi1484@gmail.com>
2024-09-04 16:06:43 +0900

    feat: 멤버 타임라인 조회 기능 구현

--- src/main/java/net/binder/api/bin/repository/BinRepository.java
+++ src/main/java/net/binder/api/bin/repository/BinRepository.java
@@ -2,12 +2,6 @@ package net.binder.api.bin.repository;
 
 import net.binder.api.bin.entity.Bin;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import java.util.Optional;
 
 public interface BinRepository extends JpaRepository<Bin, Long> {
-
-    @Query("SELECT b FROM Bin b WHERE b.deletedAt IS NULL AND b.id = :id")
-    Optional<Bin> findByIdAndNotDeleted(@Param("id") Long id);
 }
