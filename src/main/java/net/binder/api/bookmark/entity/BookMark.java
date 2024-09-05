a9c869d29283b7ebd295a6562fe89c7f7b95a1a6
horang <fnelclsrn123@gmail.com>
2024-09-05 19:45:04 +0900

    feat: 북마크 기능 초기 세팅

--- src/main/java/net/binder/api/bookmark/entity/BookMark.java
+++ src/main/java/net/binder/api/bookmark/entity/BookMark.java
@@ -1,12 +1,9 @@
-package net.binder.api.membercreatebin.entity;
+package net.binder.api.bookmark.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,29 +12,22 @@ import net.binder.api.bin.entity.Bin;
 import net.binder.api.common.entity.BaseEntity;
 import net.binder.api.member.entity.Member;
 
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
-public class MemberCreateBin extends BaseEntity {
+public class BookMark extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "bin_id")
     private Bin bin;
 
-    @Enumerated(EnumType.STRING)
-    private MemberCreateBinStatus status;
-
-    private String rejectionReason;
-
     @Builder
-    public MemberCreateBin(Member member, Bin bin, MemberCreateBinStatus status, String rejectionReason) {
+    public BookMark(Member member, Bin bin) {
         this.member = member;
         this.bin = bin;
-        this.status = status;
-        this.rejectionReason = rejectionReason;
     }
 }
