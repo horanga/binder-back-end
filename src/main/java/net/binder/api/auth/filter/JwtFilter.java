13f541a8350953212d0ffa75cc51056a37b84b09
sangchu <didi1484@gmail.com>
2024-09-06 15:42:56 +0900

    fix: jwt필터 로그 삭제

--- src/main/java/net/binder/api/auth/filter/JwtFilter.java
+++ src/main/java/net/binder/api/auth/filter/JwtFilter.java
@@ -34,7 +34,6 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        log.info("JwtFilter Start");
         String token = extractToken(request);
 
         if (token == null) {
