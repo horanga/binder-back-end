d1a96e2f38bc517abe2f3608ce9a00e0a4571391
sangchu <didi1484@gmail.com>
2024-09-06 11:28:29 +0900

    fix: 빈 조회 버그 수정

--- src/main/java/net/binder/api/bin/dto/BinDetailResponse.java
+++ src/main/java/net/binder/api/bin/dto/BinDetailResponse.java
@@ -1,32 +1,35 @@
 package net.binder.api.bin.dto;
 
+import java.time.LocalDateTime;
 import lombok.Builder;
+import lombok.Getter;
 import net.binder.api.bin.entity.Bin;
 import net.binder.api.bin.entity.BinType;
 import net.binder.api.common.dto.BaseResponse;
 
-import java.time.LocalDateTime;
-
+@Getter
 public class BinDetailResponse extends BaseResponse {
 
-    private String title;
+    private final String title;
 
-    private BinType type;
+    private final BinType type;
 
-    private double latitude;
+    private final Double latitude;
 
-    private double longitude;
+    private final Double longitude;
 
-    private String address;
+    private final String address;
 
-    private long likeCount;
+    private final Long likeCount;
 
-    private long dislikeCount;
+    private final Long dislikeCount;
 
-    private String imageUrl;
+    private final String imageUrl;
 
     @Builder
-    public BinDetailResponse(Long id, LocalDateTime createdAt, LocalDateTime modifiedAt, String title, BinType type, double latitude, double longitude, String address, long likeCount, long dislikeCount, String imageUrl) {
+    public BinDetailResponse(Long id, LocalDateTime createdAt, LocalDateTime modifiedAt, String title, BinType type,
+                             Double latitude, Double longitude, String address, Long likeCount, Long dislikeCount,
+                             String imageUrl) {
         super(id, createdAt, modifiedAt);
         this.title = title;
         this.type = type;
@@ -38,7 +41,7 @@ public class BinDetailResponse extends BaseResponse {
         this.imageUrl = imageUrl;
     }
 
-    public static BinDetailResponse from(Bin bin){
+    public static BinDetailResponse from(Bin bin) {
         return BinDetailResponse.builder()
                 .id(bin.getId())
                 .createdAt(bin.getCreatedAt())
