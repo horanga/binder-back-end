83392bf82656a8f15a1494fd88ff08ce4397f9fd
sangchu <didi1484@gmail.com>
2024-09-06 10:42:06 +0900

    fix: pathVariable long -> Long 변경

--- src/main/java/net/binder/api/memberlikebin/controller/MemberLikeBinController.java
+++ src/main/java/net/binder/api/memberlikebin/controller/MemberLikeBinController.java
@@ -5,7 +5,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import net.binder.api.common.annotation.CurrentUser;
 import net.binder.api.memberlikebin.service.MemberLikeBinService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,13 +21,13 @@ public class MemberLikeBinController {
 
     @Operation(summary = "쓰레기통 좋아요")
     @PatchMapping("/likes/{id}")
-    public void saveLike(@CurrentUser String email, @PathVariable("id") long id) {
+    public void saveLike(@CurrentUser String email, @PathVariable("id") Long id) {
         memberLikeBinService.saveLike(email, id);
     }
 
     @Operation(summary = "쓰레기통 좋아요 취소")
     @DeleteMapping("/likes/{id}")
-    public void deleteLike(@CurrentUser String email, @PathVariable("id") long id) {
+    public void deleteLike(@CurrentUser String email, @PathVariable("id") Long id) {
         memberLikeBinService.deleteLike(email, id);
     }
 }
