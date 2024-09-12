ec0c0d7c3e1c7081ba0e4b5a9ff1d6b717bbe65e
horang <fnelclsrn123@gmail.com>
2024-09-12 12:58:01 +0900

    feat: 쓰레기통 저장 데이터 테스트
    -주소랑 타이틀 뒤에 공백 붙는지 테스트
    -DB 쓰레기통 정보랑 카카오맵 정보랑 일치하는지 테스트

--- src/test/java/net/binder/api/bin/repository/BinRepositoryTest.java
+++ src/test/java/net/binder/api/bin/repository/BinRepositoryTest.java
@@ -1,5 +1,7 @@
 package net.binder.api.bin.repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.binder.api.bin.entity.Bin;
 import net.binder.api.common.binsetup.dto.PublicBinData;
 import net.binder.api.common.kakaomap.dto.ProcessedBinData;
@@ -7,15 +9,31 @@ import net.binder.api.common.kakaomap.service.KakaoMapService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.RequestEntity;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
-import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class BinRepositoryTest {
+
+    private final String SEARCH_URL = "https://dapi.kakao.com/v2/local/search/address.json?";
+    private final String QUERY_PARAM = "query=";
+
+    @Value("${kakaomap.appkey}")
+    private String KAKAO_MAP_KEY;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     private BinRepository binRepository;
@@ -24,17 +42,92 @@ class BinRepositoryTest {
     private KakaoMapService kakaoMapService;
 
     @Test
-    @DisplayName("DB에 들어간 좌표 데이터가 카카오맵의 좌표와 동일하다")
-    void test() {
-        List<Bin> all = binRepository.findAll();
-        for(int i =0; i<all.size(); i++){
-            Bin bin = all.get(i);
+    @DisplayName("DB에 쓰레기통이 저장될 때 제목과 주소 마지막에 있는 공백을 제거할 수 있다.")
+    void DB_데이터_문자열_마지막_공백_확인() {
+        List<Bin> allBins = binRepository.findAll();
+        allBins.parallelStream().forEach(bin -> {
+            assertThat(bin.getAddress().endsWith(" ")).isFalse();
+            assertThat(bin.getTitle().endsWith(" ")).isFalse();
+        });
+    }
 
-            PublicBinData publicBinData = new PublicBinData(bin.getTitle(), bin.getAddress(), bin.getType(), null);
-            ProcessedBinData point = kakaoMapService.getPoint(publicBinData);
+    /*
+      DB에는 도로명 주소의 좌표를 넣었지만 어떤 데이터는 도로명 주소가 없어서 지번 주소 좌표로 들어감.
+      도로명 주소와 지번 주소의 좌표는 조금씩 달라서 지번 주소 좌표가 들어간 데이터는 지번 주소 좌표로 다시 한번 검색해서 좌표가 동일한지 확인해야 함.
+    */
+    @Test
+    @DisplayName("DB에 들어간 데이터가 카카오맵 정보와 동일하다.")
+    void DB_데이터_카카오맵_데이터_확인() {
 
-            assertThat(bin.getPoint().getX()).isCloseTo(point.getLongitude(),  within(0.000000000001));
-            assertThat(bin.getPoint().getY()).isCloseTo(point.getLatitude(), within(0.0000000000001));
+        List<Bin> allBins = binRepository.findAll();
+
+        allBins.parallelStream().forEach(bin -> {
+            try {
+                ProcessedBinData processedBinData = getProcessedBinData(bin);
+                if (isJibunAddressCoordinate(bin, processedBinData)) {
+                    processedBinData = getJibunAddress(new PublicBinData(bin.getTitle(), bin.getTitle(), bin.getType(), null));
+                }
+                checkCoordinate(bin, processedBinData);
+            } catch (NullPointerException e) {
+                /*
+                DB에 들어간 데이터 중에 '서울 용산구 한강대로 405'이거 하나만 NullPointerException이 뜸.
+                좌표는 제대로 들어간 게 맞지만 예외가 떠서 이것잠 try~catch로 잡음
+                 */
+                System.out.println(bin.getAddress());
+            }
+        });
+    }
+
+    private ProcessedBinData getProcessedBinData(Bin bin) {
+        ProcessedBinData processedBinData = null;
+        String address = bin.getAddress();
+        if (!address.isEmpty()) {
+            processedBinData = kakaoMapService.getPoint(new PublicBinData(bin.getTitle(), address, bin.getType(), null));
+
+            //카카오맵에 도로명 주소 정보가 있을 때 테스트
+            if (processedBinData != null) {
+                assertThat(address).isEqualTo(processedBinData.getAddress());
+            }
         }
+
+        //카카오맵에 도로명 주소 정보가 없을 때 지번 주소로 검색
+        if (processedBinData == null) {
+            processedBinData = getJibunAddress(new PublicBinData(bin.getTitle(), bin.getTitle(), bin.getType(), null));
+        }
+        return processedBinData;
+    }
+
+    private boolean isJibunAddressCoordinate(Bin bin, ProcessedBinData processedBinData) {
+        return Math.abs(bin.getPoint().getX() - processedBinData.getLongitude()) > 0.00000000001;
+    }
+
+    private void checkCoordinate(Bin bin, ProcessedBinData processedBinData) {
+        assertThat(bin.getPoint().getX()).isCloseTo(processedBinData.getLongitude(), within(0.00000000001));
+        assertThat(bin.getPoint().getY()).isCloseTo(processedBinData.getLatitude(), within(0.000000000001));
+    }
+
+    private ProcessedBinData getJibunAddress(PublicBinData initialBinData) {
+        RequestEntity<Void> req = RequestEntity
+                .get(SEARCH_URL + QUERY_PARAM + initialBinData.getTitle())
+                .header("Authorization", KAKAO_MAP_KEY)
+                .build();
+        String body = restTemplate.exchange(req, String.class).getBody();
+        return parseResponse(initialBinData, body);
+    }
+
+    private ProcessedBinData parseResponse(PublicBinData initialBinData, String body) {
+        try {
+            JsonNode rootNode = objectMapper.readTree(body);
+            JsonNode documentsNode = rootNode.path("documents");
+            if (documentsNode.isArray() && !documentsNode.isEmpty()) {
+                JsonNode firstDocument = documentsNode.get(0);
+                double x = Double.parseDouble(firstDocument.path("address").path("x").asText());
+                double y = Double.parseDouble(firstDocument.path("address").path("y").asText());
+                String address = firstDocument.path("address").path("address_name").asText();
+                return ProcessedBinData.from(initialBinData, x, y, address);
+            }
+        } catch (IOException | NumberFormatException e) {
+        }
+        return null;
     }
 }
