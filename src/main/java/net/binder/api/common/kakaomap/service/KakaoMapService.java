3f2f3173ca7aeda1c842d79fb96e71d07209723a
horang <fnelclsrn123@gmail.com>
2024-09-12 12:57:04 +0900

    refactor: 쓰레기통 좌표 도로명 주소를 기반으로 한 좌표로 변경

--- src/main/java/net/binder/api/common/kakaomap/service/KakaoMapService.java
+++ src/main/java/net/binder/api/common/kakaomap/service/KakaoMapService.java
@@ -3,18 +3,15 @@ package net.binder.api.common.kakaomap.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import net.binder.api.bin.entity.BinType;
 import net.binder.api.common.binsetup.dto.PublicBinData;
 import net.binder.api.common.kakaomap.dto.ProcessedBinData;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
@@ -36,8 +33,7 @@ public class KakaoMapService {
 
     public ProcessedBinData getPoint(PublicBinData initialBinData) {
         String body = fetchDataFromApi(initialBinData);
-        ProcessedBinData initData = null;
-        return parseResponse(initialBinData, body, initData);
+        return parseResponse(initialBinData, body);
     }
 
     private String fetchDataFromApi(PublicBinData initialBinData) {
@@ -45,42 +41,32 @@ public class KakaoMapService {
                 .get(SEARCH_URL + QUERY_PARAM + initialBinData.getAddress())
                 .header("Authorization", KAKAO_MAP_KEY)
                 .build();
-        String body = restTemplate.exchange(req, String.class).getBody();
-        return body;
+        return restTemplate.exchange(req, String.class).getBody();
     }
 
-    private ProcessedBinData parseResponse(PublicBinData initialBinData, String body, ProcessedBinData initData) {
-
+    private ProcessedBinData parseResponse(PublicBinData initialBinData, String body) {
         ProcessedBinData processedBinData = null;
-
         try {
             JsonNode rootNode = objectMapper.readTree(body);
             JsonNode documentsNode = rootNode.path("documents");
             if (documentsNode.isArray() && !documentsNode.isEmpty()) {
                 JsonNode firstDocument = documentsNode.get(0);
-                double x = Double.parseDouble(firstDocument.path("x").asText());
-                double y = Double.parseDouble(firstDocument.path("y").asText());
+                double x = Double.parseDouble(firstDocument.path("road_address").path("x").asText());
+                double y = Double.parseDouble(firstDocument.path("road_address").path("y").asText());
                 String address = getAddress(initialBinData, firstDocument);
                 processedBinData = ProcessedBinData.from(initialBinData, x, y, address);
             }
         } catch (IOException | NumberFormatException e) {
         }
-
         return processedBinData;
     }
 
     private static String getAddress(PublicBinData initialBinData, JsonNode firstDocument) {
-        String roadAddress = firstDocument.path("road_address").path("address_name").asText();
-        String address=  "";
-        if(!roadAddress.isEmpty()){
-            String[] split = roadAddress.split(" ");
-
-            for(int i =1; i<split.length; i++){
-                address +=split[i]+" ";
-            }
-        } else{
+        String address = firstDocument.path("road_address").path("address_name").asText();
+        if (address.isEmpty()) {
             address = initialBinData.getAddress();
         }
+
         return address;
     }
 }
