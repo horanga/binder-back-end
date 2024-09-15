6d9e85566fe067292513d51a31190d2b342157b4
horang <fnelclsrn123@gmail.com>
2024-09-15 23:37:01 +0900

    feat: 북마크 생성 기능 추가

--- src/main/java/net/binder/api/bookmark/service/BookMarkService.java
+++ src/main/java/net/binder/api/bookmark/service/BookMarkService.java
@@ -1,6 +1,13 @@
 package net.binder.api.bookmark.service;
 
 import lombok.RequiredArgsConstructor;
+import net.binder.api.bin.entity.Bin;
+import net.binder.api.bin.service.BinService;
+import net.binder.api.bookmark.entity.Bookmark;
+import net.binder.api.bookmark.repository.BookmarkRepository;
+import net.binder.api.member.entity.Member;
+import net.binder.api.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,5 +16,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class BookMarkService {
 
+    @Autowired
+    private BinService binService;
 
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private BookmarkRepository bookmarkRepository;
+
+    public void saveBookMark(String email, Long binId){
+        Member member = memberService.findByEmail(email);
+        Bin bin = binService.findById(binId);
+
+        Bookmark bookmark = Bookmark.builder()
+                .member(member)
+                .bin(bin)
+                .build();
+        bookmarkRepository.save(bookmark);
+    }
 }
